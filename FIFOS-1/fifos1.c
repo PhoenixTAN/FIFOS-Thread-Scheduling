8586bba22a1dba7e4c0a298b558a435df0bf65b6
PhoenixTAN <tanziqi1756@outlook.com>
2020-04-03 20:42:07 -0400

    Make a ready queue.

--- FIFOS-1/fifos1.c
+++ FIFOS-1/fifos1.c
@@ -7,29 +7,42 @@
 #include "multiboot.h"
 #include "thread.h"
 #include "print.h"
+// #include "schedule.h"
 
 /* Macros. */
 /* The number of threads */ 
 #define N                       3
 #define stack_size              1024
+#define ready_queue_size        256
 
 /* Variables */
 static TCB tcb[N];
 static int uid = 0;     // counter
 
+/* data structure and operationg for ready queue */
+static int ready_queue[ready_queue_size];
+static int head;
+static int tail;
+void init_ready_queue();
+int en_queue(int tid);
+int de_queue();
+int isEmpty();
+int isFull();
+
 /* Private stack for threads */
 multiboot_uint32_t stack1[stack_size];
-multiboot_uint32_t stack2[stack_size;
+multiboot_uint32_t stack2[stack_size];
 multiboot_uint32_t stack3[stack_size];
 
 /* Forward declarations. */
-void init(multiboot_info_t* pmb);       /* entrance of the C code */
+void init(multiboot_info_t* pmb);       
 int thread_create(void* stack, void* run);
 void thread1_run();
 void thread2_run();
 void thread3_run();
 void fifo_scheduler();
 
+/* entrance of the C code */
 void init(multiboot_info_t* pmb) {
     cls();
     println("FIFOS-1: Ziqi Tan, Jiaqian Sun");
@@ -39,6 +52,21 @@ void init(multiboot_info_t* pmb) {
     create_therad(&stack1[stack_size-1], thread1_run);
     create_therad(&stack2[stack_size-1], thread2_run);
     create_therad(&stack3[stack_size-1], thread3_run);
+
+    init_ready_queue();
+    en_queue(100);
+    en_queue(255);
+    int res = de_queue();
+    if( res == 100 ) {
+        println("100");
+    }
+    if( de_queue() == 255 ) {
+        println("get 255");
+    }
+    if( isEmpty() == 1 ) {
+        println("Empty");
+    }
+
 }
 
 /* Create thread */
@@ -86,4 +114,48 @@ void fifo_scheduler() {
 
     // (5) context restore
 
+}
+
+/* Opeartions for ready queue */
+void init_ready_queue() {
+    head = 0;
+    tail = 0;
+}
+
+/* add */
+int en_queue(int tid) {
+    if( isFull() == 1 ) {
+        return -1;
+    }
+    ready_queue[tail] = tid;
+    tail = (tail + 1) % ready_queue_size;
+    println("en_queue");
+    return 0;
+}
+
+/* poll */
+int de_queue() {
+    if( isEmpty() == 1 ) {
+        return -1;
+    }
+    int res = ready_queue[head];
+    head = (head + 1) % ready_queue_size;
+    println("de_queue");
+    return res;
+}
+
+/* isEmpty */
+int isEmpty() {
+    if(head == tail ) {
+        return 1;
+    }
+    return -1;
+}
+
+/* isFull */
+int isFull() {
+     if ((tail + 1) % ready_queue_size == head ) {
+         return 1;
+     }
+     return -1;
 }
